1f2828980f53e447d835265de146e85128a5e28a
Thiago Lopes <thiagoviniciusc33@gmail.com>
2024-07-23 21:06:10 -0300

    dashboard

---
+++ src/components/weigth-chart.tsx
@@ -0,0 +1,32 @@
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+const data = [
+    { name: 'Semana 1', weight: 70, goal: 68 },
+    { name: 'Semana 2', weight: 69.5, goal: 68 },
+    { name: 'Semana 3', weight: 69, goal: 68 },
+    { name: 'Semana 4', weight: 68.5, goal: 68 },
+    { name: 'Semana 5', weight: 68, goal: 68 },
+    { name: 'Semana 6', weight: 67.5, goal: 68 },
+    { name: 'Semana 7', weight: 67, goal: 68 },
+    { name: 'Semana 8', weight: 66.5, goal: 68 },
+    { name: 'Semana 9', weight: 66, goal: 68 },
+    { name: 'Semana 10', weight: 65.5, goal: 68 },
+    { name: 'Semana 11', weight: 65, goal: 68 },
+    { name: 'Semana 12', weight: 64.5, goal: 68 },
+  ];
+  
+
+export function WeightChart() {
+  const minWeight = Math.min(...data.map(d => d.weight));
+  const maxWeight = Math.max(...data.map(d => d.weight));
+
+  return (
+    <LineChart width={600} height={300} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis domain={[minWeight - 1, maxWeight + 1]} tickCount={5} /> {/* Ajustar o domínio */}
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+      <Line type="monotone" dataKey="goal" stroke="#82ca9d" />
+    </LineChart>
+  );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,6 @@
 import { WeatherCard } from "../components/weather-card";
+import { WeightChart } from "../components/weigth-chart";
+import { Grid } from '@mui/material';
 
 
   export function Dashboard() {
@@ -6,9 +8,18 @@ import { WeatherCard } from "../components/weather-card";
 
   
     return (
-      <>
-        <WeatherCard />
-
-      </>
+      <Grid 
+        container 
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={6}>
+          <WeatherCard />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <WeightChart />
+        </Grid>
+      </Grid>
     );
   }
